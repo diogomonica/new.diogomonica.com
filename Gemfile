3e7d019a10c6cccfbc39036bd7cc2823fac0f8a3
Diogo Monica <diogo.monica@gmail.com>
2015-12-20 21:41:59 -0800

    Updating bundle and sync script

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'jekyll'
 gem 'jekyll-sitemap'
+gem 'jekyll-paginate'
 gem 'pry'
 gem 'kramdown'
 gem 'rouge'
--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 rm ./images/timeline/*; cp ./images/timeline_original/* ./images/timeline/ ; cd ./images/timeline; sips -Z 300 *.png *.jpg ; cd ../..
-bundle exec jekyll b; rsync -avz -e "ssh -p 311" _site/ bear.diogomonica.com:/var/www/diogomonica.com
+bundle exec jekyll b; rsync -avz -e "ssh -p 311" _site/ burly.diogomonica.com:/var/www/diogomonica.com
