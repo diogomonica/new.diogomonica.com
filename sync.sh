f2143b33b0fcd8dc9a85d2c82a9b413a1b121b9d
Diogo Monica <diogo.monica@gmail.com>
2015-12-29 13:18:44 -0800

    Adding opengraph

--- sync.sh
+++ sync.sh
@@ -1,5 +1,6 @@
-rm -rf css/* ; rm -rf _assets/sass/*.css ; rm -rf _assets/sass/*.map; sass -C --style compressed --update _assets/sass ; cp _assets/sass/*.css _assets/stylesheets/ ; cp _assets/stylesheets/* css/
+rm -rf css/* ; rm -rf _assets/sass/*.css ; rm -rf _assets/sass/*.map; sass -C --style compressed --update _assets/sass ; cp _assets/sass/*.css css/ ; cp _assets/css/* css/
 rm ./images/timeline/*; cp ./images/timeline_original/* ./images/timeline/ ; cd ./images/timeline; sips -Z 300 *.png *.jpg ; cd ../..
 bundle exec jekyll b
 chmod -R o+rx _site/
 rsync -avz -e "ssh -p 311" _site/ burly.diogomonica.com:/var/www/diogomonica.com
+rm -rf css/* ; rm -rf _site/ ; rm -rf _assets/sass/*.css ; rm -rf _assets/sass/*.map
